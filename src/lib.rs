945cee07fd6a5275aa21209b0f882656c8b4b217
Jonathan Reem <jonathan.reem@gmail.com>
2015-02-11 20:02:46 -0800

    (feat) Add mods to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(unboxed_closures, core, io, hash, os)]
 #![cfg_attr(test, deny(warnings))]
+#![cfg_attr(test, feature(test))]
 
 //! Blazingly fast non-blocking IO.
 
@@ -11,11 +12,11 @@ extern crate log;
 
 extern crate event;
 extern crate mio;
+extern crate syncbox;
 
-pub use std::path;
-
-pub use path::{
-    Path
+pub use iostream::{
+    IoReadStream,
+    IoWriteStream
 };
 
 pub use error::{
@@ -37,10 +38,15 @@ pub mod error;
 pub mod sys;
 pub mod constants;
 pub mod iostream;
+pub mod eventstream;
+pub mod net;
+pub mod util;
 pub mod buf {
     pub use mio::buf::{Buf, MutBuf, RingBuf,
                        RingBufReader, RingBufWriter};
 }
 
 mod register;
+mod internals;
+mod impls;
 
